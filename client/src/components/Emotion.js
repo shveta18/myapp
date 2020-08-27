6dbe99eaa817c71f39a89a09907df6aea82a0335
Shveta Kumar <sk@shveta.ca>
2020-08-27 17:42:03 -0400

    added mongodb connection and server connection

---
+++ client/src/components/Emotion.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Emotion extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>emotional wellbeing component</p>
+        
+      </div>
+    );
+  }
+}
+
+export default Emotion;
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://wellbeing:Ev7qc4m*U8K9XV@ds031627.mlab.com:31627/heroku_nft88rdw'
+}
